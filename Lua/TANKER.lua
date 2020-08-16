b9c1693ae8a5cbcc76a8842292e4bbddefcd5656
Thierry Michel Philippe Kleist <thierry@kle.ist>
2020-08-16 17:25:32 +0200

    feat: added menu to change altitude for Shell

---
+++ Lua/TANKER.lua
@@ -0,0 +1,23 @@
+do
+    -- Get groups
+  local _shell = Group.getByName("TANKER Blue #002#001")
+
+  -- Get the tanker down
+  function tankerShellActionsLow()
+  trigger.action.pushAITask(_shell, 1)
+  trigger.action.outText("Shell1-1 going to 18 angels", 5)
+  end
+
+  function tankerShellActionsHigh()
+  trigger.action.pushAITask(_shell, 2)
+  trigger.action.outText("Shell1-1 going to 25 angels", 5)
+  end
+
+  -- ADD SUB RADIO MENUs
+  local tankerMenu = missionCommands.addSubMenu("Tanker")
+  local shellMenu = missionCommands.addSubMenu("Shell1-1", tankerMenu)
+
+  -- ADD MENU ITEMS
+  missionCommands.addCommand("Request low orbit", shellMenu, tankerShellActionsLow, {})
+  missionCommands.addCommand("Request high (default) orbit", shellMenu, tankerShellActionsHigh, {})
+end
